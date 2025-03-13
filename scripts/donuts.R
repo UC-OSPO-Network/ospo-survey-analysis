612f55120b01ac1bb2e58edcb412ac750553ab70
Virginia Scarlett <virginiascarlett@msa-189.library.ucsb.edu>
2025-03-13 11:08:45 -0700

    improve flexibility of donuts script

--- scripts/donuts.R
+++ scripts/donuts.R
@@ -1,62 +1,84 @@
-# Purpose: create donut charts reflecting overall participation by group
+# Purpose: create donut charts reflecting response rates by group
 
 suppressWarnings(suppressMessages(source("utils.R")))
 
+
+create_df_for_plotting <- function(data, column) {
+  # Ensure the column exists in the data frame
+  if (!column %in% names(data)) {
+    stop("Column not found in data frame")
+  }
+
+  # Extract specified column and remove empty strings
+  values <- data[[column]][nzchar(data[[column]])]
+  # Count occurrences of each unique value
+  values_table <- table(values)
+
+  # At this point we are essentially creating a single stacked bar with all our data.
+  # Then we will put it on a polar coordinate system to make it a donut chart.
+  # Method from https://r-graph-gallery.com/128-ring-or-donut-plot.html
+
+  # Convert to data frame and compute fractions
+  long_data <- as.data.frame(values_table) %>%
+    mutate(fraction = Freq / sum(Freq)) %>%
+    arrange(desc(fraction))
+
+  # Compute the cumulative percentages (top of each rectangle)
+  long_data$ymax <- cumsum(long_data$fraction)
+  long_data$ymin <- c(0, head(long_data$ymax, n = -1))
+
+  # Compute label position
+  long_data$labelPosition <- (long_data$ymax + long_data$ymin) / 2
+
+  # Create label column
+  long_data$label <- paste0(long_data$Freq, "\n(", round(long_data$fraction * 100, 1), "%)")
+
+  return(long_data)
+}
+
+donut_chart <- function(df) {
+  ggplot(df, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = values)) +
+    geom_rect() +
+    geom_text(x = 3.5, aes(y = labelPosition, label = label), size = 6, color = "white") + # Add labels
+    scale_fill_manual(values = colors) +
+    theme_void() +
+    coord_polar(theta = "y") +
+    xlim(c(2, 4)) +
+    theme(
+      legend.text = element_text(size = 14),
+      legend.title = element_blank(),
+      plot.title = element_text(hjust = 0.5, size = 20),
+      plot.background = element_rect(fill = "white", color = "white")
+    )
+}
+
+
+
+
+
+
+
 data <- load_qualtrics_data("deidentified_no_qual.tsv")
 
-# Select job column and remove empty strings
-jobs <- data$job_category[nzchar(data$job_category)]
-jobs <- table(jobs) # count occurrences of each job
-
-long_data <- data.frame(jobs) %>%
-  mutate(fraction = Freq / sum(Freq)) %>%
-  arrange(desc(fraction))
+job_data <- create_df_for_plotting(data, "job_category")
 
 # Clean up this one long job name
-long_data$jobs <- gsub(
+job_data$values <- gsub(
   "Other research staff \\(e.g., research scientist, research software engineer\\)",
   "Other research staff",
-  long_data$jobs
+  job_data$values
 )
 
-# At this point we are essentially creating a single stacked bar with all our data.
-# Then we will put it on a polar coordinate system to make it a donut chart.
-# Method from https://r-graph-gallery.com/128-ring-or-donut-plot.html
+campus_data <- create_df_for_plotting(data, "campus")
 
-# Compute the cumulative percentages (top of each rectangle)
-long_data$ymax <- cumsum(long_data$fraction)
+p1 <- donut_chart(job_data) +
+  labs(title = "Job Category of Respondents")
 
-# Compute the bottom of each rectangle
-long_data$ymin <- c(0, head(long_data$ymax, n = -1))
+p2 <- donut_chart(campus_data) +
+  labs(title = "Campus of Respondents")
 
-# Compute label position
-long_data$labelPosition <- (long_data$ymax + long_data$ymin) / 2
+# Combine the two plots side by side
+p1 + p2
 
 
-# Create a donut chart
-
-ggplot(long_data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = jobs)) +
-  geom_rect() +
-  # geom_label(x = 3.5, aes(y = labelPosition, label = label), size = 6) +
-  scale_fill_manual(values = colors) +
-  theme_void() +
-  coord_polar(theta = "y") +
-  xlim(c(2, 4))
-
-# Create label column
-long_data$label <- paste0(long_data$Freq, "\n(", round(long_data$fraction * 100, 1), "%)")
-
-ggplot(long_data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = jobs)) +
-  geom_rect() +
-  geom_text(x = 3.5, aes(y = labelPosition, label = label), size = 6, color = "white") + # Add labels
-  scale_fill_manual(values = colors) +
-  theme_void() +
-  coord_polar(theta = "y") +
-  xlim(c(2, 4)) +
-  theme(
-    legend.text = element_text(size = 14),
-    legend.title = element_blank()
-  )
-
-
-save_plot("donut.tiff", 8, 6)
+save_plot("donut.tiff", 16, 12)
